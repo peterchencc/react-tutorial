e4a29fa648fde765c06d5b495d68f7b33594eb1e
peterchencc <peterchencc@gmail.com>
2019-10-20 22:17:06 +0800

    Updates

---
+++ precache-manifest.73cc1c41b64a7327c826192a37718c2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8918d2a1ffa6223f71b416cbde4f58ba",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "6ecf7002fef61f0d71f2",
+    "url": "/react-tutorial/static/css/main.8ce9cc50.chunk.css"
+  },
+  {
+    "revision": "4e5d43f9f4cc8f7f39f1",
+    "url": "/react-tutorial/static/js/2.176bf385.chunk.js"
+  },
+  {
+    "revision": "6ecf7002fef61f0d71f2",
+    "url": "/react-tutorial/static/js/main.be8684f9.chunk.js"
+  },
+  {
+    "revision": "5543c54dfff0a1bd80cd",
+    "url": "/react-tutorial/static/js/runtime-main.d5b13df3.js"
+  }
+]);
